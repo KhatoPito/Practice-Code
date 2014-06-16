052e24679fce2b3cbf2410cdf1c7165f2ba644e2
KhatoPito <ved2210@users.noreply.github.com>
2014-06-15 21:03:27 -0700

    ReverseWordsInSentence

--- Strings/ReverseWordInSentence.cs
+++ Strings/ReverseWordInSentence.cs
@@ -1,59 +1,55 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace reverseWords
 {
-	public class Sample
-	{
+    public class ReverseWordsInSentence
+    {
 
-		public static void Main(string[] args)
-		{
-			Console.WriteLine(reverseWords("sample of baroda"));
-			Console.ReadLine();
-		}
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(reverseWords("sample of baroda"));
+            Console.ReadLine();
+        }
 
-		public static string reverseWords(String s)
-		{
-			char[] stringChar = s.ToCharArray();
-			int length = stringChar.Length, tempIndex = 0;
+        public static string reverseWords(String s)
+        {
+            char[] stringChar = s.ToCharArray();
+            int length = stringChar.Length, tempIndex = 0;
 
-			Swap(stringChar, 0, length - 1);
+            Swap(stringChar, 0, length - 1);
 
-			for (int i = 0; i < length; i++)
-			{
-				if (i == length-1)
-				{
-					Swap(stringChar, tempIndex, i);
-					tempIndex = i + 1;
-				}
-				else if (stringChar[i] == ' ')
-				{
-					Swap(stringChar, tempIndex, i-1);
-					tempIndex = i + 1;
-				}
-			}
+            for (int i = 0; i < length; i++)
+            {
+                if (i == length - 1)
+                {
+                    Swap(stringChar, tempIndex, i);
+                    tempIndex = i + 1;
+                }
+                else if (stringChar[i] == ' ')
+                {
+                    Swap(stringChar, tempIndex, i - 1);
+                    tempIndex = i + 1;
+                }
+            }
 
-			return new String(stringChar);
-		}
+            return new String(stringChar);
+        }
 
-		private static void Swap(char[] p, int startIndex, int endIndex)
-		{
-			int sampleIndex = endIndex;
+        private static void Swap(char[] p, int startIndex, int endIndex)
+        {
+            while (startIndex < endIndex)
+            {
+                p[startIndex] ^= p[endIndex];
+                p[endIndex] ^= p[startIndex];
+                p[startIndex] ^= p[endIndex];
 
-			while (startIndex < endIndex)
-			{
-				p[startIndex] ^= p[endIndex];
-				p[endIndex] ^= p[startIndex];
-				p[startIndex] ^= p[endIndex];
-
-				startIndex++;
-				endIndex--;
-			}
-
-			startIndex = sampleIndex + 1;
-		}
-	}
+                startIndex++;
+                endIndex--;
+            }
+        }
+    }
 
 }
