c58e2658a96d8a19728066b72c386bbbb425cfdb
KhatoPito <KhatoPito@users.noreply.github.com>
2016-05-21 16:06:45 -0700

    Update FindFirstUniqueChar.cs

--- Strings/FindFirstUniqueChar.cs
+++ Strings/FindFirstUniqueChar.cs
@@ -7,7 +7,23 @@ using System.Threading.Tasks;
 namespace FindFirstUniqueChar
 {
     //•	Find the first non-repeating character in a string:("AAAAAAAAAAABCA" -> B ) 
-
+    
+    // NOTE - Update - instead of traversing the entire string again, we can keep track of the index of character
+   /* Think of the case when a string has miilions of characters - maintaing index for the first occurance along with the
+    count will perfrom much better 
+    
+    The first part of the algorithm runs through the string
+    to construct the count array (in O(n) time). This is reasonable. But the second part about running through the string again
+    just to find the first non-repeater is not good in practice. In real situations, your string is expected to be much larger than 
+    your alphabet. Take DNA sequences for example: they could be millions of letters long with an alphabet of just 4 letters. 
+    What happens if the non-repeater is at the end of the string? Then we would have to scan for a long time (again).
+    We can augment the count array by storing not just counts but also the index of the
+    st time you encountered the character e.g. (3, 26) for ‘a’ meaning that ‘a’ got counted 3 times and the first time 
+    it was seen is at position 26. So when it comes to finding the first non-repeater, we just have to scan the count array, 
+    instead of the string. Thanks to Ben for suggesting this approach.
+    
+    http://www.geeksforgeeks.org/given-a-string-find-its-first-non-repeating-character/
+*/
     class FindFirstUniqueChar
     {
         private static char ReturnFirstUniqueChar(string sampleString)
