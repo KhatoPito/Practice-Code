64ef13112f3765c9560dc43ee40f10c246161b10
KhatoPito <KhatoPito@users.noreply.github.com>
2018-12-26 18:36:24 -0800

    9. Palindrome Number

---
+++ LeetCode/Arrays/IsPalindrome.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KhatoPito
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Solution9 sol = new Solution9();
+            Console.WriteLine(sol.IsPalindrome(1210));
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution9 {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+                x = -x;
+            else if (x == 0)
+                return true;
+
+            int result = 0, num = x;
+            while (num > 0)
+            {
+                result = result * 10 + num % 10;
+                num = num / 10;
+            }
+            
+            return result == x;
+        }
+    }
+
+}
