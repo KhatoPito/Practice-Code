732279038f44637d83be5cb60eaae8f7c8811086
KhatoPito <KhatoPito@users.noreply.github.com>
2019-04-26 01:03:25 -0700

    125. Valid Palindrome

---
+++ LeetCode/Strings/IsPalindrome.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+/*
+     *Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+    Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+    Example 1:
+
+    Input: "A man, a plan, a canal: Panama"
+    Output: true
+    Example 2:
+
+    Input: "race a car"
+    Output: false
+ *
+ */
+namespace KhatoPito.StringPractice
+{
+    // i/p Console.WriteLine(IsPalindrome.CheckPalindrome("A man, a plan, a canal: Panama"));
+    public static class IsPalindrome
+    {
+        public static bool CheckPalindrome(string s)
+        {
+            char[] charArray = s.ToLower().ToCharArray();
+            int start = 0; int end = s.Length - 1;
+
+            while (start < end)
+            {
+                if(char.IsLetterOrDigit(charArray[start]) && char.IsLetterOrDigit(charArray[end]))
+                {
+                    if (charArray[start++] != charArray[end--])
+                        return false;
+                }
+                else
+                {
+                    if (!char.IsLetterOrDigit(charArray[start]))
+                        start++;
+
+                    if (!char.IsLetterOrDigit(charArray[end]))
+                        end--;
+                }
+             }
+
+            return true;
+        }
+    }
+}
